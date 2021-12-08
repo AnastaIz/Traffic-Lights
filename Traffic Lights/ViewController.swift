3b84fc09b884c96c6f0f77f3c4bfa683322ae950
Anastasia Izmaylova <abulatova1992@gmail.com>
2021-12-08 21:55:27 +0300

    Base Pattern

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    
+    @IBOutlet var yellowView: UIView!
+    
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 50
+        yellowView.layer.cornerRadius = 50
+        greenView.layer.cornerRadius = 50
+        
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
     }
 
-
+    
+    
 }
 
